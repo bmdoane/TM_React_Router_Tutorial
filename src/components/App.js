70ec6c8902798bf3a58b877c50f86c42f7b113b5
Brian Doane <brianmdoane@gmail.com>
2019-08-12 13:59:51 -0500

    Source code starter for RR tut

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import Home from './Home'
+import Players from './Players'
+import Teams from './Teams'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        React Router
+      </div>
+    )
+  }
+}
+
+export default App;
