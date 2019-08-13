f332b911438a8621741b9b5b8a4beeae5014ef47
Brian Doane <brianmdoane@gmail.com>
2019-08-12 22:42:29 -0500

    Fetching team names and populating to Sidebar comp and showing sidebar instruction if no team has been selected.

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
 
 export default class Teams extends Component {
+  state = {
+    teamNames: [],
+    loading: true,
+  }
+  componentDidMount() {
+    getTeamNames()
+      .then((teamNames) => {
+        this.setState(() => ({
+          loading: false,
+          teamNames,
+        }))
+      })
+  }
   render() {
+    const { loading, teamNames } = this.state
+    const { location, match } = this.props
+
     return (
       <div className='container two-column'>
-        Teams
+        <Sidebar
+          loading={loading}
+          title='Teams'
+          list={teamNames}
+          {...this.props}
+        />
+
+        {loading === false && location.pathname === '/teams'
+          ? <div className='sidebar-instruction'>Select a team</div>
+          : null}
       </div>
     )
   }
